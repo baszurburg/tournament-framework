c1d81c236ed692207fb359df4ad73104f40d8ec1
baszurburg <bas.zurburg@xs4all.nl>
2017-10-03 15:28:28 +0200

    start with editing a profile

---
+++ tournament-app/src/app/settings/profile/profile-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tma-profile-edit',
+  templateUrl: './profile-edit.component.html',
+  styles: []
+})
+export class ProfileEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
